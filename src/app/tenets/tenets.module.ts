397660d5c787b4223213a19721be67346f076f3e
sponyibe <sponyibe@yahoo.com>
2019-09-18 18:57:20 -0400

    new update

---
+++ src/app/tenets/tenets.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TenetsPage } from './tenets.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TenetsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TenetsPage]
+})
+export class TenetsPageModule {}
